af2017b7938f2c61bcec5bfb597bf087ac38b091
EdmundGoodman <egoodman3141@gmail.com>
2021-06-13 16:29:57 +0100

    Started re-write of code

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from cardModel.cards import *
-
+from cardModel.cards import Suits, Faces, Card, Pile, Deck
 
 
 class Player:
@@ -7,121 +6,47 @@ class Player:
         self.name = name
         self._cardsHeld = set()
         self._cardsNotHeld = set()
-        self._facesHeld = set()
         self._setsWon = set()
+        self._facesHeld = set()
+        self._maxCardsForFace = {face:3 for face in Faces}
+        #self._minCardsForFace = {face:0 for face in Faces}
 
+    
     def give(self, card):
-        if card in self._cardsHeld:
-            self._cardHeld.remove(card)
-            self._cardsNotHeld.add(card)
-            #Some way of tracking cards around to see if faces held can be decremented?
-            return card
-        else:
-            return None
-
-    def ask(self, cardAsked, player):
-        card = player.ask(cardAsked)
         if card is not None:
+            #The player no longer holds the card
+            self._cardHeld.remove(card)
+            #The player doesn't hold the card
+            self._cardsNotHeld.add(card)
+            maxCardsForFace = self._maxCardsForFace[card.face]
+            if maxCardsForFace > 0:
+                #The player holds at least one fewer cards of that face than
+                #the maximum they could have started with
+                self._maxCardsForFace[card.face] -= 1
+            if maxCardsForFace == 0 and card.face in self._facesHeld:
+                #If the player has given away all cards of a face from the
+                #maximum they could have started with, they must not hold any of
+                #that face anymore
+                self._facesHeld.remove(card.face)
+
+    def ask(self, card, received, otherPlayers):
+        if received:
             self._cardsHeld.add(card)
-            if card in self._cardsNotHeld:
-                self._cardsNotHeld.remove(card)
-            self._facesHeld.add(card.face) #Implicitly true, but ignore this
-            #Handle set removal
+            self._facesHeld.add(card.face)
+            if self._maxCardsForFace < 3:
+                self._maxCardsForFace += 1
+            
+
         else:
-            self._cardsNotHeld.add(cardAsked)
-            self._facesHeld.add(cardAsked.face)
+            #The player cannot hold a card they just asked for, and they
+            #didn't receive, so they don't hold it now
+            self._cardsNotHeld.add(card)
+            #The player must hold at least one card of that face to have asked
+            #for that card
+            self._facesHeld.add(card.face)
 
-    def getSetsInUsableHand(self):
-        justFaces = [card.face for card in self._cardsHeld]
-        return [faces for faces in set(justFaces) if justFaces.count(faces) == 4]
-
-        """for faceOfSet in facesOfSets:
-            #Do this for all players!!!!
-            self._setsWon.add(faceOfSet)
-            self._facesHeld.remove
-            for suit in Suits:
-                self._cardsHeld.add()
-
-
-    def updateFacesWon(self):
-        cardFacesList = [card.face for card in self._cardsHeld]
-        for face in set(cardFacesList):
-            #If full set of one face is held
-            if cardFacesList.count(face) == 4:
-                #Put the face in the list of face sets won, and remove all the
-                #cards of that face from the player's hand
-                self._facesWon.add(face)
-                for suit in Suits:
-                    self._cardsHeld.remove(Card(face, suit))
-
-                """
-
-    def __str__(self):
-        out =  "{}:\n".format(self.name)
-        out += "\tHolds the cards: {}\n".format(self._cardsHeld)
-        out += "\tDoesn't hold the cards: {}\n".format(self._cardsNotHeld)
-        out += "\tHolds the faces: {}\n".format(", ".join([face.name for face
-                                                 in self._facesHeld]))
-        out += "\tHas won the sets: {}\n".format(", ".join([face.name for face
-                                                  in self._setsWon]))
-        return out
-
-
-    def __repr__(self):
-        return str(self)
-
-class You(Player):
-    def __init__(self, cards=[]):
-        super().__init__("You")
-        self._cardsHeld.update(set(cards))
-        self._cardsNotHeld.update(set(Deck() - Pile(cards)))
-        self._facesHeld = set([card.face for card in cards])
-
-
-
-class Game:
-    def __init__(self):
-        self.deck = Deck()
-
-    def start(numPlayers, numCardslkj):
-        self.players = self.getPlayers(numPlayers, numCards)
-
-    def getPlayers(self, numPlayers, numCards):
-        """Enter your cards and position, then initialise all the players
-        in the data structures"""
-        players = []
-
-        #Enter your cards and your position in the course of play
-        yourCards = self.getYourCards()
-        yourPosition = self.getYourPosition()
-
-        #Create players in order of the course of play, indcluding you
-        for i in range(numPlayers):
-            if i == yourPosition-1:
-                players.append(You(yourCards))
-            else:
-                playerName = input("Enter player {}'s name: ".format(i+1))
-                players.append(Player(playerName))
-        return players
-
-
-if __name__=="__main__":
-
-    g = Game()
-
-    """p = Player("Edmund")
-    p._cardsHeld.update(set([
-        Card(Faces.ACE, Suits.HEARTS),
-        Card(Faces.ACE, Suits.SPADES),
-    ]))
-    p._cardsNotHeld.update(set([
-        Card(Faces.FIVE, Suits.HEARTS),
-        Card(Faces.SIX, Suits.SPADES),
-    ]))
-    p._facesHeld.update(set([
-        Faces.EIGHT
-    ]))
-    p._setsWon.update(set([
-        Faces.NINE
-    ]))
-    print(p)"""
+            if self._maxCardsForFace == 0:
+                #If we have tracked them to not have any cards of this face,
+                #complain about it
+                print("The program has tracked them losing all cards of that")
+                print("face. Please check if they have made a mistake")
---
+++ app2.py
@@ -0,0 +1,128 @@
+from cardModel.cards import *
+
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self._cardsHeld = set()
+        self._cardsNotHeld = set()
+        self._facesHeld = set()
+        self._setsWon = set()
+
+    def give(self, card):
+        if card in self._cardsHeld:
+            self._cardHeld.remove(card)
+            self._cardsNotHeld.add(card)
+            #Some way of tracking cards around to see if faces held can be decremented?
+            return card
+        else:
+            return None
+
+    def ask(self, cardAsked, player):
+        card = player.ask(cardAsked)
+        if card is not None:
+            self._cardsHeld.add(card)
+            if card in self._cardsNotHeld:
+                self._cardsNotHeld.remove(card)
+            self._facesHeld.add(card.face) #Implicitly true, but ignore this
+            #Handle set removal
+        else:
+            self._cardsNotHeld.add(cardAsked)
+            self._facesHeld.add(cardAsked.face)
+
+    def getSetsInUsableHand(self):
+        justFaces = [card.face for card in self._cardsHeld]
+        return [faces for faces in set(justFaces) if justFaces.count(faces) == 4]
+
+        """for faceOfSet in facesOfSets:
+            #Do this for all players!!!!
+            self._setsWon.add(faceOfSet)
+            self._facesHeld.remove
+            for suit in Suits:
+                self._cardsHeld.add()
+
+
+    def updateFacesWon(self):
+        cardFacesList = [card.face for card in self._cardsHeld]
+        for face in set(cardFacesList):
+            #If full set of one face is held
+            if cardFacesList.count(face) == 4:
+                #Put the face in the list of face sets won, and remove all the
+                #cards of that face from the player's hand
+                self._facesWon.add(face)
+                for suit in Suits:
+                    self._cardsHeld.remove(Card(face, suit))
+
+                """
+
+    def __str__(self):
+        out =  "{}:\n".format(self.name)
+        out += "\tHolds the cards: {}\n".format(self._cardsHeld)
+        out += "\tDoesn't hold the cards: {}\n".format(self._cardsNotHeld)
+        out += "\tHolds the faces: {}\n".format(", ".join([face.name for face
+                                                 in self._facesHeld]))
+        out += "\tHas won the sets: {}\n".format(", ".join([face.name for face
+                                                  in self._setsWon]))
+        return out
+
+
+    def __repr__(self):
+        return str(self)
+
+
+class You(Player):
+    def __init__(self, cards=[]):
+        super().__init__("You")
+        self._cardsHeld.update(set(cards))
+        self._cardsNotHeld.update(set(Deck() - Pile(cards)))
+        self._facesHeld = set([card.face for card in cards])
+
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+
+    def start(self, numPlayers, numCards):
+        self.players = self.getPlayers(numPlayers, numCards)
+
+    def getPlayers(self, numPlayers, numCards):
+        """Enter your cards and position, then initialise all the players
+        in the data structures"""
+        players = []
+
+        #Enter your cards and your position in the course of play
+        yourCards = self.getYourCards()
+        yourPosition = self.getYourPosition()
+
+        #Create players in order of the course of play, including you
+        for i in range(numPlayers):
+            if i == yourPosition-1:
+                players.append(You(yourCards))
+            else:
+                playerName = input("Enter player {}'s name: ".format(i+1))
+                players.append(Player(playerName))
+        return players
+
+
+
+
+if __name__=="__main__":
+    g = Game()
+
+    """p = Player("Edmund")
+    p._cardsHeld.update(set([
+        Card(Faces.ACE, Suits.HEARTS),
+        Card(Faces.ACE, Suits.SPADES),
+    ]))
+    p._cardsNotHeld.update(set([
+        Card(Faces.FIVE, Suits.HEARTS),
+        Card(Faces.SIX, Suits.SPADES),
+    ]))
+    p._facesHeld.update(set([
+        Faces.EIGHT
+    ]))
+    p._setsWon.update(set([
+        Faces.NINE
+    ]))
+    print(p)"""
